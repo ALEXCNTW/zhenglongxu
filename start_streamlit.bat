ec7423307bb49d7d705612059837865c26be3c11
ALEXCNTW <zxu84@stevens.edu>
2025-04-14 21:02:15 -0400

    Update start_streamlit.bat

--- start_streamlit.bat
+++ start_streamlit.bat
@@ -1,14 +1,24 @@
 @echo off
+echo -------------------------------------
+echo Checking if virtual environment exists...
+if not exist ".venv" (
+    echo Creating virtual environment...
+    python -m venv .venv
+)
+
+echo -------------------------------------
 echo Activating virtual environment...
 call .venv\Scripts\activate
 
-echo Installing required packages from requirements.txt...
+echo -------------------------------------
+echo Installing dependencies from requirements.txt...
+pip install --upgrade pip
 pip install -r requirements.txt
 
-echo Starting Streamlit app without file watch...
+echo -------------------------------------
+echo Starting Streamlit app...
 set STREAMLIT_WATCH_FILE_SYSTEM=false
-streamlit run main_streamlit.py --server.runOnSave false
+set STREAMLIT_SERVER_RUN_ON_SAVE=false
+streamlit run main_streamlit.py
 
 pause
-
-
